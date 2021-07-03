50e9add9d529f4c1c17a02d2ddaf936296dc7cb4
BlazejakPiotr <piotr.blazejak@yahoo.com>
2021-07-03 19:44:30 +0200

    applying random question&answers into nodes

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
 window.onload = function(){
-    createQuestionContainer()
-    createAnswerContainers()
+    startGame()
 }
 
-const userScore = 0
-
-// TODO : 
-// Create an array of questions with nested array with answers
-
-const myObj = {
+const quizObj = {
     firstQuestion : {
         question : 'What is the result of 2 + 2 ?',
         correctAnswer : '4',
@@ -18,31 +12,49 @@ const myObj = {
         question : 'How we call function named "foo"?',
         correctAnswer : 'foo()',
         incorrectAnswers : ['function foo()', 'foo', 'call function foo()']
+    },
+    thirdQuestion : {
+        question : 'What "DOM" means?',
+        correctAnswer : 'Document Object Model',
+        incorrectAnswers : ['Something nice', 'I dont really know', 'Maybe ill go back to school?']
+    },
+    fourthQuestion : {
+        question : 'What is HTML?',
+        correctAnswer : 'HyperText Markup Language',
+        incorrectAnswers : ['Something nice', 'I dont really know', 'Maybe ill go back to school?']
     }
 }
 
 
-let getMainTag = document.getElementsByTagName('main')
+const getMainTag = document.getElementsByTagName('main')
+const userScore = 0
 
-// Creating question container
-const createQuestionContainer = function(){
+// Starting quiz
+const startGame = function(){
+    let questionArr = Object.entries(quizObj) // Converting quizObj into an array
+    let getRandom = Math.floor(Math.random() * questionArr.length) // Generating random question
 
-    let createQuestionNode = document.createElement('div')
-    createQuestionNode.id = 'question-container'
-    createQuestionNode.innerText = myObj.firstQuestion.question 
-    getMainTag[0].appendChild(createQuestionNode)    
-}
-
-// Creating four blank answer containers
-const createAnswerContainers = function(){
-    for (let i = 0; i < 4; i++){
-        let createAnswerNode = document.createElement('div')
-        createAnswerNode.classList.add('answer')
-        createAnswerNode.innerText = myFunction()
-        getMainTag[0].appendChild(createAnswerNode)
+    // Creating question node and applying text to it
+    const createQuestionContainer = function(){
+        let createQuestionNode = document.createElement('div')
+        createQuestionNode.id = 'question-container'
+        createQuestionNode.innerText = questionArr[getRandom][1].question
+        getMainTag[0].appendChild(createQuestionNode)    
     }
+
+    // Creating answer nodes and applaying text to it
+    const createAnswerContainers = function(){
+        questionArr[getRandom][1].incorrectAnswers.push(questionArr[getRandom][1].correctAnswer) // Pushing correct answer into incorrect answers array
+
+        for (let i = 0; i < 4; i++){
+            let createAnswerNode = document.createElement('div')
+            createAnswerNode.classList.add('answer')
+            createAnswerNode.innerText = questionArr[getRandom][1].incorrectAnswers[i] // NEED FIX! Appending answer by index makes correct answer always as last
+            getMainTag[0].appendChild(createAnswerNode)
+        }
+    }
+    createQuestionContainer()
+    createAnswerContainers()
 }
 
-const myFunction = function(){
-    alert('whatsaaap')
-}
+
